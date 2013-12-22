179a1c50cee062fa57609ecdda3cbfcba9b8801f
Dominic Hamon <dma+github@stripysock.com>
2013-12-22 13:55:07 -0800

    unit needed for benchmark time flag

--- benchmark.sh
+++ benchmark.sh
@@ -6,13 +6,13 @@ export GOPATH=`pwd`
 
 echo "qserver"
 # BenchmarkRead runs too long if --benchtime > ~0.2
-go test qserver --bench=. --benchtime=0.2
+go test qserver --bench=. --benchtime=200ms
 
 ./qserver --port=4242 &
 PID=$!
 
 echo "qclient"
-go test qclient --port=4242 --host=localhost --bench=. --benchtime=0.5
+go test qclient --port=4242 --host=localhost --bench=.
 
 echo "killing qserver"
 kill $PID
